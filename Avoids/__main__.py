18671b986899f3ae4e04c76a0c741888d1e1fd7c
Toto99-cpu <suraniani543@gmail.com>
2026-09-14 01:56:05 +0700

    add Avoids/__main__.py

---
+++ Avoids/__main__.py
@@ -0,0 +1,23 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from Avoids import app, LOGGER
+from Avoids.modules import ALL_MODULES
+
+
+async def avoid():
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"Avoids.modules.{all_module}")
+    LOGGER.info(f"Successfully loaded {len(ALL_MODULES)}.")
+    LOGGER.info("Bot Started")
+    await idle()
+
+    try:
+        await app.stop()
+    except:
+        pass
+    LOGGER.info("Stopping! GoodBye")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(avoid())
